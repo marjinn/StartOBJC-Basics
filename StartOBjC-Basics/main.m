23c78ba5f02852dae54958821836c8155c15b213
mar Jinn <marjinn@mars-Mac.local>
2013-12-07 04:18:42 +0530

    Initial Commit

---
+++ StartOBjC-Basics/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  StartOBjC-Basics
+//
+//  Created by mar Jinn on 12/7/13.
+//  Copyright (c) 2013 mar Jinn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
